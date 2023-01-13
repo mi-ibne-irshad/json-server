f55c8d761527fe5e71b85aadfce5628e8f4a6a4c
m-islam-irshad <muhammad.islam@invozone.com>
2023-01-14 03:02:58 +0500

    Practice code of Json-Server

---
+++ users.js
@@ -0,0 +1,21 @@
+// import { faker } from '@faker-js/faker';
+const faker = require('@faker-js/faker/locale/de');
+
+
+const USERS = [];
+
+const createRandomUser = () => {
+  return {
+    userId: faker.datatype.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+Array.from({ length: 10 }).forEach(() => {
+  USERS.push(createRandomUser());
+});
